0cdfa3fba528de9b15cf9e8b76aaebb2d3e3bf34
anreh <anurehka@gmail.com>
2022-07-12 11:00:32 +0800

    Commit 20220712 1059 - added drawable resources - added dimens - added styles - lateinit changes

--- app/src/main/java/com/example/myhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/myhelloworld/MainActivity.kt
@@ -3,25 +3,39 @@ package com.example.myhelloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImg: ImageView
+    lateinit var numberText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val rollButton: Button = findViewById(R.id.rollButton)
+        diceImg = findViewById(R.id.diceImg)
+        numberText = findViewById(R.id.numberText)
+//        val rollButton: Button = findViewById(R.id.rollButton)
+        val rollButton = findViewById<Button>(R.id.rollButton)
         rollButton.setOnClickListener{ rollDice()}
     }
 
     private fun rollDice(){
-
-
         val randomNumb = (1..6).random()
-
-        val numberText: TextView = findViewById(R.id.numberText)
+//        val numberText: TextView = findViewById(R.id.numberText)
+//        val diceImg:ImageView = findViewById(R.id.diceImg)
         numberText.text = randomNumb.toString()
+
+        val imgSrc = when (randomNumb){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImg.setImageResource(imgSrc)
+
         Toast.makeText(this, randomNumb.toString(),
             Toast.LENGTH_SHORT).show()
     }
